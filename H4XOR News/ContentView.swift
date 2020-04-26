d7171a091ab7ffe62bbeee9f907a1701a676ee7f
sudhakar <sudhakarh1999@gmail.com>
2020-04-26 16:25:07 +0530

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by sudhakar H on 26/04/20.
+//  Copyright © 2020 sudhakar H. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
